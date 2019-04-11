9a881a2c709fb0b4062d245d9db6699ef933f298
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-04-11 03:17:16 -0700

    add persistence with local storage

--- js/app.js
+++ js/app.js
@@ -9,9 +9,6 @@ var imagesOnPage = imageContainer.getElementsByTagName('img');
 // stores all images created in an array
 var imageLibrary = [];
 
-// keeps track of total number of clicks 
-var totalClicks = 0;
-
 // max number of clicks allowed
 var maxTotalClicks = 25;
 
@@ -21,21 +18,38 @@ var previousImagesSelections = [];
 // holds indices of images in current selection Options
 var currentImageSelections = [];
 
+var getStorageVotes = function(){
+  var votes = JSON.parse(localStorage.getItem('votes'));
+  return votes;
+};
 
+var setStorageVotes = function(votes){
+  localStorage.setItem('votes', JSON.stringify(votes));
+};
 
 // Image constructor with name of image, and format (jgp, gif, png)
 var Image = function(name, format) {
   this.name = name;
   this.filePath = `img/${name}.${format}`;
-  this.numClicks = 0;
   this.numTimesDisplayed = 0;
+
+  this.numClicks = function(){
+    var imageNames = JSON.parse(localStorage.getItem('imageNames'));
+    return getStorageVotes()[imageNames.indexOf(this.name)];
+  };
+
+  var isInitialized = JSON.parse(localStorage.getItem('initialized'));
+  if(!isInitialized) {
+    var votes = getStorageVotes();
+    votes.push(0);
+    setStorageVotes(votes);
+    
+    //imageNames array keeps track of names for all images by index number in image library - declared in barchart.js
+    var storedImageNames = JSON.parse(localStorage.getItem('imageNames'));
+    storedImageNames.push(name);
+    localStorage.setItem('imageNames', JSON.stringify(storedImageNames));
+  }
   imageLibrary.push(this);
-
-  //votes array keeps tally for all images by index number in image library - declared in barchart.js
-  votes.push(0);
-
-  //imageNames array keeps track of names for all images by index number in image library - declared in barchart.js
-  imageNames.push(name);
 };
 
 // generates a random number between min and max inclusive
@@ -47,8 +61,9 @@ var generateRandomNumber = function (min, max) {
 var getNewImageSelections = function() {
   var counter = 0;
   var randomImageIndex;
+  console.log(imageLibrary);
   while (counter < 3) {
-    randomImageIndex = generateRandomNumber(0, imageLibrary.length -1);
+    randomImageIndex = generateRandomNumber(0, imageLibrary.length - 1);
     // check for dupes in image container and previous image container with current selection
     if (previousImagesSelections.includes(randomImageIndex) || currentImageSelections.includes(randomImageIndex)){
       randomImageIndex = generateRandomNumber(0, imageLibrary.length -1);
@@ -76,9 +91,11 @@ var displayImageStats = function(){
   var imageStatsContainer = document.getElementById('image-stats');
   var ulEl = document.createElement('ul');
   var liEl;
-  for(var i = 0; i < imageLibrary.length; i++) {
+  var images = JSON.parse(localStorage.getItem('imageNames'));
+  var votes = getStorageVotes();
+  for(var i = 0; i < images.length; i++) {
     liEl = document.createElement('li');
-    liEl.textContent = `${imageLibrary[i].numClicks} votes for ${imageLibrary[i].name}`;
+    liEl.textContent = `${votes[i]} votes for ${images[i]}`;
     ulEl.appendChild(liEl);
   }
   imageStatsContainer.appendChild(ulEl);
@@ -89,21 +106,27 @@ var handleUserClick = function(event) {
 
   // get image clicked
   var imageCickedId = event.target.id;
-  var imageCicked = imageLibrary[imageCickedId];
+  //var imageCicked = imageLibrary[imageCickedId];
 
+  var votes = getStorageVotes();
+  
   // add 1 to image stats
-  imageCicked.numClicks++;
-
-  //add 1 to total votes fir pic index in votes array
   votes[imageCickedId]++;
+  setStorageVotes(votes);
 
-  // add 1 to total clicks
-  totalClicks++;
+  var totalClicksInStorage = JSON.parse(localStorage.getItem('totalClicks'));
+
+  if (totalClicksInStorage < maxTotalClicks) {
+    // add 1 to total clicks
+    totalClicksInStorage++;
+  }
+
+  localStorage.setItem('totalClicks', JSON.stringify(totalClicksInStorage));
 
   // update bar chart
-  votesBarChart.update();
+  //votesBarChart.update();
 
-  if(totalClicks === maxTotalClicks){
+  if(totalClicksInStorage === maxTotalClicks){
     console.log('Max clicks submitted. Removing event listener...');
     imageContainer.removeEventListener('click', handleUserClick);
     displayImageStats();
@@ -121,31 +144,55 @@ var handleUserClick = function(event) {
   }
 };
 
-// add event listener on userClick
-imageContainer.addEventListener('click', handleUserClick);
+var initializeImageLibrary = function() {
+  // create images for image library
+  new Image('bag', 'jpg');
+  new Image('banana', 'jpg');
+  new Image('bathroom', 'jpg');
+  new Image('boots', 'jpg');
+  new Image('breakfast', 'jpg');
+  new Image('bubblegum', 'jpg');
+  new Image('chair', 'jpg');
+  new Image('cthulhu', 'jpg');
+  new Image('dog-duck', 'jpg');
+  new Image('dragon', 'jpg');
+  new Image('pen', 'jpg');
+  new Image('pet-sweep', 'jpg');
+  new Image('scissors', 'jpg');
+  new Image('shark', 'jpg');
+  new Image('sweep', 'png');
+  new Image('tauntaun', 'jpg');
+  new Image('unicorn', 'jpg');
+  new Image('usb', 'gif');
+  new Image('water-can', 'jpg');
+  new Image('wine-glass', 'jpg');
+};
 
-// create images for image library
-new Image('bag', 'jpg');
-new Image('banana', 'jpg');
-new Image('bathroom', 'jpg');
-new Image('boots', 'jpg');
-new Image('breakfast', 'jpg');
-new Image('bubblegum', 'jpg');
-new Image('chair', 'jpg');
-new Image('cthulhu', 'jpg');
-new Image('dog-duck', 'jpg');
-new Image('dragon', 'jpg');
-new Image('pen', 'jpg');
-new Image('pet-sweep', 'jpg');
-new Image('scissors', 'jpg');
-new Image('shark', 'jpg');
-new Image('sweep', 'png');
-new Image('tauntaun', 'jpg');
-new Image('unicorn', 'jpg');
-new Image('usb', 'gif');
-new Image('water-can', 'jpg');
-new Image('wine-glass', 'jpg');
+if (!localStorage.getItem('totalClicks')) {
+  localStorage.setItem('totalClicks', '0');
+  localStorage.setItem('votes', JSON.stringify([]));
+  localStorage.setItem('imageNames', JSON.stringify([]));
+  initializeImageLibrary();
+  localStorage.setItem('initialized', 'true');
 
-getNewImageSelections();
-showNewImageSelections();
-drawChart();
+  getNewImageSelections();
+  showNewImageSelections();
+  // add event listener on userClick
+  imageContainer.addEventListener('click', handleUserClick);
+} else {
+  var totalClicks = JSON.parse(localStorage.getItem('totalClicks'));
+  if(totalClicks === maxTotalClicks){
+    alert('you have reached the max votes');
+    console.log('Max clicks submitted. No event listener...');
+    displayImageStats();
+    showChart();
+  } else {
+    imageContainer.addEventListener('click', handleUserClick);
+    initializeImageLibrary();
+    getNewImageSelections();
+    showNewImageSelections();
+  }
+}
+
+
+
